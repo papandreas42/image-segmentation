0808fb5d44eb7088cdee5984b6e763da85695120
A. Papanikolaou <andreaspap42@gmail.com>
2023-11-30 23:00:39 +0100

    Submit first job

---
+++ job_script.sh
@@ -0,0 +1,30 @@
+#!/bin/sh 
+
+
+### General LSF options 
+[...] refer to official HPC guide
+### -- end of LSF options --
+
+# Load environment variables
+source ./.env
+
+# Create job_out if it is not present
+if [[ ! -d ${REPO}/job_out ]]; then
+	mkdir ${REPO}/job_out
+fi
+
+date=$(date +%Y%m%d_%H%M)
+mkdir ${REPO}/runs/train/${date}
+
+# Activate venv
+module load python3/3.10.12
+module load cuda/12.1
+source ${REPO}/.env/bin/activate
+
+# Exit if previous command failed
+if [[ $? -ne 0 ]]; then
+	exit 1
+fi
+
+# run training
+python3 early-stopping.py
