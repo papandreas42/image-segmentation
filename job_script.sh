390d6981b5a444ab20175443f1eb8521036eb059
A. Papanikolaou <andreaspap42@gmail.com>
2023-11-30 23:15:53 +0100

    update general lsf options

--- job_script.sh
+++ job_script.sh
@@ -1,9 +1,32 @@
 #!/bin/sh 
 
 
-### General LSF options 
-[...] refer to official HPC guide
-### -- end of LSF options --
+### General options
+### –- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J testjob
+### -- ask for number of cores (default: 1) --
+#BSUB -n 4
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 00:21
+# request 5GB of system-memory
+#BSUB -R "rusage[mem=5GB]"
+### -- set the email address --
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u your_email_address
+### -- send notification at start --
+### #BSUB -B
+### -- send notification at completion--
+### #BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
+#BSUB -o gpu_%J.out
+#BSUB -e gpu_%J.err
+# -- end of LSF options --
 
 # Load environment variables
 source ./.env_var
