777efe1c5bd3a93ca8b7d5315a2efcf7f4eb55e6
A. Papanikolaou <andreaspap42@gmail.com>
2023-12-01 14:27:03 +0100

    Fix issue with rw file in python

--- multiple_jobs_script.py
+++ multiple_jobs_script.py
@@ -1,12 +1,14 @@
 import subprocess
 
 def set_n_train_in_var_file(n_train):
-    with open('./env_var', 'rw') as file:
+    with open('./env_var', 'r') as file:
         # read a list of lines into data
         data = file.readlines()
-
         data[2] = "export N_TRAIN=" + n_train + "\n"
-        file.writelines( data )
+
+    with open('./env_var', 'w') as file:
+    # read a list of lines into data
+        file.writelines(data)
 
 
 for i in range(1,5):
