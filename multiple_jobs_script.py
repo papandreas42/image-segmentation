16e14e41d661346695c9c2e7827d7b912c87d1cc
A. Papanikolaou <andreaspap42@gmail.com>
2023-12-01 14:28:39 +0100

    typo fix

--- multiple_jobs_script.py
+++ multiple_jobs_script.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def set_n_train_in_var_file(n_train):
-    with open('./env_var', 'r') as file:
+    with open('./.env_var', 'r') as file:
         # read a list of lines into data
         data = file.readlines()
         data[2] = "export N_TRAIN=" + n_train + "\n"
@@ -14,6 +14,6 @@ def set_n_train_in_var_file(n_train):
 for i in range(1,5):
     set_n_train_in_var_file(i)
     print(subprocess.run(["source",
-                          './env_var'], shell=True))
+                          './.env_var'], shell=True))
     print(subprocess.run(["echo",
                           '$N_TRAIN'], shell=True))
